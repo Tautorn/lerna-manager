70a63f20485c07188b4b948dd2cd5869b661206e
Tautorn <brunocarneirofs@gmail.com>
2017-03-13 22:43:42 -0300

    exporting modules

--- packages/dota/index.js
+++ packages/dota/index.js
@@ -1,1 +1,15 @@
-require("core")
+'use strict';
+
+const core = require("core")
+
+console.log('\nFlag:container')
+
+core._container()
+
+console.log('\nFlag:nav-bar')
+
+core._navBar()
+
+console.log('\nFlag:tooltip')
+
+core._tooltip()
